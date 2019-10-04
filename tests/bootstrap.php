0edc527354da788b11d469399abec9f68f8b4446
Carlos C Soto <eclipxe13@gmail.com>
2019-10-04 10:42:53 -0500

    Set default time zone to America/Mexico_City

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,5 +5,8 @@ declare(strict_types=1);
 // report all errors
 error_reporting(-1);
 
+// timezone
+date_default_timezone_set('America/Mexico_City');
+
 // require composer autoloader
 require_once __DIR__ . '/../vendor/autoload.php';
