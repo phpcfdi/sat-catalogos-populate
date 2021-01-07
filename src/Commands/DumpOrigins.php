76f95bdf9d73a7d030afd526b704fa366f953cc8
Carlos C Soto <eclipxe13@gmail.com>
2021-01-07 10:51:27 -0600

    Change code style on DumpOrigins::run for better reading

--- src/Commands/DumpOrigins.php
+++ src/Commands/DumpOrigins.php
@@ -16,49 +16,20 @@ class DumpOrigins implements CommandInterface
         $common = 'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos';
         $origins = new Origins([
             new Origin('CFDI', "{$common}/catCFDI.xls"),
-            new Origin('Nóminas', 'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/catNomina.xls'),
+            new Origin('Nóminas', "{$common}/catNomina.xls"),
             new Origin('CCE - Claves de pedimento', "{$common}/c_ClavePedimento.xls"),
-            new Origin(
-                'CCE - Colonias',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_Colonia.xls'
-            ),
-            new Origin(
-                'CCE - Entidades o estados',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/C_Estado.xls'
-            ),
-            new Origin(
-                'CCE - Fracciones arancelarias',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_FraccionArancelaria.xls'
-            ),
-            new Origin(
-                'CCE - Incoterms',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_INCOTERM.xls'
-            ),
-            new Origin(
-                'CCE - Localidades',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_Localidad.xls'
-            ),
-            new Origin(
-                'CCE - Motivo traslado',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_MotivoTraslado.xls'
-            ),
-            new Origin(
-                'CCE - Municipios',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_Municipio.xls'
-            ),
-            new Origin(
-                'CCE - Tipos de operaciones',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_TipoOperacion.xls'
-            ),
-            new Origin(
-                'CCE - Unidades de medida',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/c_UnidadAduana.xls'
-            ),
-            new Origin(
-                'REP',
-                'http://omawww.sat.gob.mx/tramitesyservicios/Paginas/documentos/catPagos.xls'
-            ),
+            new Origin('CCE - Colonias', "{$common}/c_Colonia.xls"),
+            new Origin('CCE - Entidades o estados', "{$common}/C_Estado.xls"),
+            new Origin('CCE - Fracciones arancelarias', "{$common}/c_FraccionArancelaria.xls"),
+            new Origin('CCE - Incoterms', "{$common}/c_INCOTERM.xls"),
+            new Origin('CCE - Localidades', "{$common}/c_Localidad.xls"),
+            new Origin('CCE - Motivo traslado', "{$common}/c_MotivoTraslado.xls"),
+            new Origin('CCE - Municipios', "{$common}/c_Municipio.xls"),
+            new Origin('CCE - Tipos de operaciones', "{$common}/c_TipoOperacion.xls"),
+            new Origin('CCE - Unidades de medida', "{$common}/c_UnidadAduana.xls"),
+            new Origin('REP', "{$common}/catPagos.xls"),
         ]);
+
         echo (new OriginsIO())->originsToString($origins) . PHP_EOL;
 
         return 0;
