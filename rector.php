cc67603a93c70563992af43f9a777e7f070e6a00
Carlos C Soto <eclipxe13@gmail.com>
2021-09-04 21:49:24 -0500

    Ensure PHP 7.3

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\SetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddMethodCallBasedStrictParamTypeRector;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    // get parameters
+    $parameters = $containerConfigurator->parameters();
+
+    // Define what rule sets will be applied
+    $containerConfigurator->import(SetList::PHP_73);
+
+    // get services (needed for register a single rule)
+    $services = $containerConfigurator->services();
+
+    // register a single rule
+//    $services->set(Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector::class);
+//    $services->set(AddMethodCallBasedStrictParamTypeRector::class)->call('configure', [[
+//        AddMethodCallBasedStrictParamTypeRector::TRUST_DOC_BLOCKS => true,
+//    ]]);
+};
